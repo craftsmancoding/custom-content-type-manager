0d1f4157fdfbca9798388f08fcd4b580b0febb35
Everett Griffiths <everett@fireproofsocks.com>
2015-05-06 00:44:08 -0700

    Working on svn publish bash script

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: fireproofsocks
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=FABHDKPU7P6LN
 Tags: cms, content management, custom post types, custom content types, custom fields, images, image fields, ecommerce, modx
 Requires at least: 3.3.0
-Tested up to: 3.8.1
+Tested up to: 4.2.1
 Stable tag: 0.9.7.13
 Version: 0.9.8.0
 
--- to-svn.sh
+++ to-svn.sh
@@ -1,8 +1,78 @@
 #!/bin/bash
 
 # This is a helper script that takes our git-controlled code here and ultimately commits it to the SVN repo for this
-# plugin (defined below).
+# plugin (defined below as the SVNDIR).
 #
-# 1.
+# USAGE
+# -------------
+# Basic usage:
+#   bash to-svn.sh
+#
+# Tag and release new version
+#   bash to-svn.sh 1.2.3
+#
+# 1. "export" the current directory to a temp directory
+# 2. merge the temp directory in with the managed SVN directory
+# 3. commit the SVN code
 
-git status
+
+#################
+# CONFIGURATION
+#################
+# Temp dir -- will be deleted!
+# Omit trailing slash.
+# Don't use spaces or weird chars in the dir names.
+TMPDIR="/tmp/cctm"
+
+# Relative to this dir
+SVNDIR="../../cctm.svn"
+
+
+# Resolve SVNDIR to an absolute dir
+# One-liner similar to PHP's __DIR__ -- omits trailing slash
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+cd $DIR;
+cd $SVNDIR;
+SVNDIR=`pwd`;
+
+
+# Verify Data
+if [[ -d $SVNDIR ]] && [[ -d ${SVNDIR}/.svn ]]; then
+    echo "SVN repository directory detected at ${SVNDIR}"
+else
+    echo "${SVNDIR} is not an SVN working copy.";
+    exit 1;
+fi
+
+
+# Prep the tmp dir
+rm -rf $TMPDIR;
+# We want rsync to use the trailing slash for referencing this directory, otherwise a sub-dir will be created
+rsync -arvz ${DIR}/ $TMPDIR --exclude=".git" --exclude=".gitignore" --exclude="*.sh" --exclude=".idea";
+
+# Update the version?
+if [[ -z $1 ]]; then
+    # We need something in here for the code block to be valid
+    echo "Version number NOT updated."
+else
+    echo "Updating the version to ${1}";
+    cd $TMPDIR;
+    sed -i '' "s/Version:.*/Version: ${1}/" index.php
+    sed -i '' "s/Stable tag:.*/Stable tag: ${1}/" readme.txt
+    sed -i '' "s/Version:.*/Version: ${1}/" readme.txt
+    sed -i '' "s/.*const version = .*/    const version = '${1}'\;/" includes/CCTM.php
+fi
+
+# Move it all from tmp into svn
+rsync -arvz ${TMPDIR}/ $SVNDIR;
+
+
+# Commit
+# svn info ${SVNDIR} | grep 'URL:' | sed -e 's/URL: //'
+
+# Copy from trunk to tag
+
+
+# SUCCESS
+exit 0;
